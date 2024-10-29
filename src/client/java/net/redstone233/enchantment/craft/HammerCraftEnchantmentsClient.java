a63af9f1d045ce680fd8f118881d28608ef05fd4
Redstone2337 <3356168312@qq.com>
2024-10-29 17:26:25 +0800

    更新 craft

---
+++ src/client/java/net/redstone233/enchantment/craft/HammerCraftEnchantmentsClient.java
@@ -0,0 +1,10 @@
+package net.redstone233.enchantment.craft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HammerCraftEnchantmentsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
