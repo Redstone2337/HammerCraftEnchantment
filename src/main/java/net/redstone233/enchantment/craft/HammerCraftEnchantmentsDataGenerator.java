67313e7541c6d7c68d040d38e6c1feb69f7b1d6a
Redstone2337 <3356168312@qq.com>
2024-10-30 09:17:47 +0800

    更新 HammerCraftEnchantmentsDataGenerator.java

--- src/main/java/net/redstone233/enchantment/craft/HammerCraftEnchantmentsDataGenerator.java
+++ src/main/java/net/redstone233/enchantment/craft/HammerCraftEnchantmentsDataGenerator.java
@@ -2,6 +2,7 @@ package net.redstone233.enchantment.craft;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.redstone233.enchantment.craft.data.EnglishLanguageProvider;
 
 public class HammerCraftEnchantmentsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
